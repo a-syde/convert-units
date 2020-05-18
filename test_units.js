d854e8bae83addc41ada2acdff479be26d5ccabe
Ali Almustafa <ali.almustafa.de@gmail.com>
2020-05-18 15:57:52 +0200

    Correct calculations

--- test_units.js
+++ test_units.js
@@ -1,4 +1,9 @@
 const convert = require('./lib/index')
 
-console.log(convert(1).from('l/s').to('l/h'));
-console.log(convert(1).from('l/s').to('gal/h'));
+// console.log(convert(100).from('kg m').to('mt km'));
+// console.log(convert(100).from('kg m').to('t mi'));
+
+// console.log(convert(100).from('l/kg m').to('l/mt km'));
+console.log(convert(100).from('l/kg m').to('l/mt km'));
+console.log(convert(100).from('l/kg m').to('gal/t mi'));
+
