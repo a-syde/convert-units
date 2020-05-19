376112033611e0300f6d94a073cd0283443f68db
Ali Almustafa <ali.almustafa.de@gmail.com>
2020-05-19 13:46:04 +0200

    Correct kg/m

--- test_units.js
+++ test_units.js
@@ -4,6 +4,6 @@ const convert = require('./lib/index')
 // console.log(convert(100).from('kg m').to('t mi'));
 
 // console.log(convert(100).from('l/kg m').to('l/mt km'));
-console.log(convert(100).from('l/kg m').to('l/mt km'));
-console.log(convert(100).from('l/kg m').to('gal/t mi'));
+// console.log(convert(100).from('l/kg m').to('l/mt km'));
+// console.log(convert(100).from('l/kg m').to('gal/t mi'));
 
